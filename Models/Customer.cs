bfc6148477284a997094b0ee991a4a575763ff05
mehdikordilo <mehdi.kordilo@gmail.com>
2023-12-14 04:08:43 +0330

    Customer orm model added

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,10 +1,8 @@
-
-
 namespace webapi.Models
 {
     public class Customer
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public DateTime DateOfBirth { get; set; }

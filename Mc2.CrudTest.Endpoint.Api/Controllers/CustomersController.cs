e0529dd22c4859e865bc56bb8f8506c92de829d3
Mortaza Daneshyar <thisissorna@yahoo.com>
2024-01-10 08:27:16 +0330

    Add web api controller

---
+++ Mc2.CrudTest.Endpoint.Api/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using Ardalis.Result;
+using Mc2.CrudTest.Application.Contract.Customers.Commands;
+using Mc2.CrudTest.Application.Contract.Customers.Queries;
+using Mc2.CrudTest.Application.Contract.Customers.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mc2.CrudTest.Endpoint.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CustomersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<Result<CreatedCustomerResponse>> Create([FromBody] CreateCustomerCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    [HttpPut]
+    public async Task<Result> Update([FromBody] UpdateCustomerCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    [HttpDelete("{Id:guid}")]
+    public async Task<Result> Delete([FromRoute] DeleteCustomerCommand command)
+    {
+         return await _mediator.Send(command);
+    }
+
+    [HttpGet("{Id:guid}")]
+    public async Task<Result<GetCustomerByIdResponse>> Get([FromRoute] GetCustomerByIdQuery query)
+    {
+        return await _mediator.Send(query);
+    }
+}
---
+++ Mc2.CrudTest.Endpoint.Api/Program.cs
@@ -0,0 +1,33 @@
+using Mc2.CrudTest.Application.DI;
+using Mc2.CrudTest.Infrastructure.Persistence.DI;
+using Mc2.CrudTest.Infrastructure.Query.DI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationLayer();
+builder.Services.AddPersistenceLayer(builder.Configuration);
+builder.Services.AddQueryLayer(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

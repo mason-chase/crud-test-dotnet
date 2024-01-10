d5a81a55094026622dac8e93ad267fe23c03397b
Danial Shokouhmanesh <danialshokouhmanesh@outlook.com>
2024-01-10 21:42:49 +0330

    init

---
+++ Mc2.CrudTest.Presentation/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Mc2.CrudTest.Presentation.Shared
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}

4060e54693e763376e330cbcecd9a145a8044fe8
Mortaza Daneshyar <thisissorna@yahoo.com>
2024-01-10 08:28:45 +0330

    Add migration for readonly dbcontext

---
+++ Mc2.CrudTest.Infrastructure.Query/Migrations/20240109191314_AddIndex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mc2.CrudTest.Infrastructure.Query.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"CREATE UNIQUE CLUSTERED INDEX [ucidx_customer_id] ON [dbo].CustomersView([Id]);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"DROP INDEX [CustomersView].[ucidx_customer_id];");
+        }
+    }
+}

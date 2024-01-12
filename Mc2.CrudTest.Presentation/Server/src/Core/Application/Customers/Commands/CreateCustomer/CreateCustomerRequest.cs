72538ae1a21a124257e0bf697a8c3fefef2c95e0
Danial Shokouhmanesh <danialshokouhmanesh@outlook.com>
2024-01-12 11:41:18 +0330

    feat: required atrebutes added on requests

--- Mc2.CrudTest.Presentation/Server/src/Core/Application/Customers/Commands/CreateCustomer/CreateCustomerRequest.cs
+++ Mc2.CrudTest.Presentation/Server/src/Core/Application/Customers/Commands/CreateCustomer/CreateCustomerRequest.cs
@@ -1,11 +1,19 @@
-﻿namespace Application.Customers.Commands.CreateCustomer;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Customers.Commands.CreateCustomer;
 
 public class CreateCustomerRequest
 {
+    [Required]
     public string Firstname { get; set; } = string.Empty;
+    [Required]
     public string Lastname { get; set; } = string.Empty;
+    [Required]
     public DateTime DateOfBirth { get; set; }
+    [Required]
     public string PhoneNumber { get; set; } = string.Empty;
+    [Required]
     public string Email { get; set; } = string.Empty;
+    [Required]
     public string BankAccountNumber { get; set; } = string.Empty;
 }
--- Mc2.CrudTest.Presentation/Server/src/Core/Application/Customers/Commands/UpdateCustomer/UpdateCustomerRequest.cs
+++ Mc2.CrudTest.Presentation/Server/src/Core/Application/Customers/Commands/UpdateCustomer/UpdateCustomerRequest.cs
@@ -1,13 +1,22 @@
-﻿namespace Application.Customers.Commands.UpdateCustomer
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Customers.Commands.UpdateCustomer
 {
     public class UpdateCustomerRequest
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Firstname { get; set; } = string.Empty;
+        [Required]
         public string Lastname { get; set; } = string.Empty;
+        [Required]
         public DateTime DateOfBirth { get; set; }
+        [Required]
         public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string BankAccountNumber { get; set; } = string.Empty;
     }
 }

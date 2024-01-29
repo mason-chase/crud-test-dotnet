0f859947c83161aaf46c2ab04070b08e7b856c40
MohammadMobasher <m.mobasher.z@gmail.com>
2024-01-29 23:47:07 +0330

    init ServiceCollectionExtensions

---
+++ Mc2.CrudTest.Presentation/Server/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mc2.CrudTest.Presentation.Server.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        internal static IServiceCollection AddDatabase(
+            this IServiceCollection services,
+            IConfiguration configuration)
+            => services
+                .AddDbContext<CrudContext>(options => options
+                    .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    }
+}

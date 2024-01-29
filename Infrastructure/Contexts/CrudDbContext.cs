e05627e355811c63de6f99545173f9efaf5bc6d9
MohammadMobasher <m.mobasher.z@gmail.com>
2024-01-29 23:48:12 +0330

    init CrudDbContext

---
+++ Infrastructure/Contexts/CrudDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts
+{
+    public class CrudDbContext :DbContext
+    {
+        public CrudDbContext(DbContextOptions<CrudDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

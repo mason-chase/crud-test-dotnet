555fa86adac0457cd0d64a5af5a11b2575e90f52
MohammadMobasher <m.mobasher.z@gmail.com>
2024-01-30 18:09:24 +0330

    add Customer entity

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,40 @@
+﻿namespace Domain.Entities
+{
+    public class Customer
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// FirstName
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// LastName
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// DateOfBirth
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// PhoneNumber
+        /// </summary>
+        public ulong PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// BankAccountNumber
+        /// </summary>
+        public string BankAccountNumber { get; set; }
+    }
+}

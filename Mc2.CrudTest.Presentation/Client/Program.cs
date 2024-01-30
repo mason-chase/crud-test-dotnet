9990f057af72547bb90e8dcb88f93b96d84d9479
MohammadMobasher <m.mobasher.z@gmail.com>
2024-01-30 18:43:05 +0330

    register MudBlazor in Program.cs

--- Mc2.CrudTest.Presentation/Client/Program.cs
+++ Mc2.CrudTest.Presentation/Client/Program.cs
@@ -1,4 +1,4 @@
-using Mc2.CrudTest.Presentation.Client;
+using MudBlazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,6 +11,8 @@ namespace Mc2.CrudTest.Presentation.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddMudServices();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 

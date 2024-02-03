a71cc43cafea8ba769e4013a66f1fd59dec8d0ef
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 16:59:17 +0330

    create DTO for customer in client module

---
+++ Mc2.CrudTest.Presentation/Client/Models/Customers/CustomerDTO.cs
@@ -0,0 +1,40 @@
+﻿namespace Mc2.CrudTest.Presentation.Client.Models.Customers
+{
+    public class CustomerDTO
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// FirstName
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// LastName
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// DateOfBirth
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// PhoneNumber
+        /// </summary>
+        public ulong PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// BankAccountNumber
+        /// </summary>
+        public string BankAccountNumber { get; set; }
+    }
+}
---
+++ Mc2.CrudTest.Presentation/Client/Models/Customers/UpdateCustomerDTO.cs
@@ -0,0 +1,48 @@
+﻿using Mc2.CrudTest.Presentation.Shared.SSOT;
+
+namespace Mc2.CrudTest.Presentation.Client.Models.Customers
+{
+    public class UpdateCustomerDTO
+    {
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// FirstName
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// LastName
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// DateOfBirth
+        /// </summary>
+        public string DateOfBirth { get; set; }
+
+        /// <summary>
+        /// PhoneNumber
+        /// </summary>
+        public ulong PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Country
+        /// </summary>
+        public CountrySSOT Country { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// BankAccountNumber
+        /// </summary>
+        public string BankAccountNumber { get; set; }
+    }
+}

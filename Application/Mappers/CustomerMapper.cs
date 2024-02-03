fd159179f8e6d64a4df156819da3beb72b5e99b8
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 16:13:59 +0330

    create mapper for customer

---
+++ Application/Mappers/CustomerMapper.cs
@@ -0,0 +1,19 @@
+﻿using Application.Models;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappers
+{
+    public class CustomerMapper : Profile
+    {
+        public CustomerMapper()
+        {
+
+            CreateMap<Customer, CustomerDTO>();
+
+            CreateMap<CreateCustomerDTO, Customer>();
+
+            CreateMap<UpdateCustomerDTO, Customer>();
+        }
+    }
+}

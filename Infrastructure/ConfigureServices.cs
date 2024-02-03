d739b4eb3238fc6e27725a0af483d2f422c977f3
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 16:23:28 +0330

    register dbContext in Infrastructure and remove it from server module

--- Infrastructure/ConfigureServices.cs
+++ Infrastructure/ConfigureServices.cs
@@ -1,14 +1,30 @@
 ﻿using Application.Interfaces.Repositories;
+using Infrastructure.Contexts;
 using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
 {
     public static class ConfigureServices
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
         {
-            return services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddDbContext<CrudDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                builder => builder.MigrationsAssembly(typeof(CrudDbContext).Assembly.FullName));
+
+                var dbContext = new CrudDbContext(options.Options);
+                if (dbContext.Database.GetPendingMigrations().Any())
+                {
+                    dbContext.Database.Migrate();
+                }
+            });
+            return services;
         }
     }
 }

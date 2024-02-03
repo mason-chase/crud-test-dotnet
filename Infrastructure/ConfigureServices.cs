327f480acabedff9528cbc195d5e8dd53396748a
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 17:27:28 +0330

    register dbcontext in memory

--- Infrastructure/ConfigureServices.cs
+++ Infrastructure/ConfigureServices.cs
@@ -5,25 +5,33 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace Infrastructure
 {
     public static class ConfigureServices
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
-            IConfiguration configuration)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddTransient<ICustomerRepository, CustomerRepository>();
-            services.AddDbContext<CrudDbContext>(options =>
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
             {
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
-                builder => builder.MigrationsAssembly(typeof(CrudDbContext).Assembly.FullName));
-
-                var dbContext = new CrudDbContext(options.Options);
-                if (dbContext.Database.GetPendingMigrations().Any())
+                services.AddDbContext<CrudDbContext>(options =>
+                    options.UseInMemoryDatabase("CrudTest"));
+            }
+            else
+            {
+                services.AddTransient<ICustomerRepository, CustomerRepository>();
+                services.AddDbContext<CrudDbContext>(options =>
                 {
-                    dbContext.Database.Migrate();
-                }
-            });
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    builder => builder.MigrationsAssembly(typeof(CrudDbContext).Assembly.FullName));
+
+                    var dbContext = new CrudDbContext(options.Options);
+                    if (dbContext.Database.GetPendingMigrations().Any())
+                    {
+                        dbContext.Database.Migrate();
+                    }
+                });
+            }
             return services;
         }
     }

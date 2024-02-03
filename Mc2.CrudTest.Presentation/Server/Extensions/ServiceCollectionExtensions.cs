d0a0724a4cb08efb6b39f93423be921c1c24eff4
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 16:18:07 +0330

    register dbContext in ServiceCollectionExtensions in Server modul

--- Mc2.CrudTest.Presentation/Server/Extensions/ServiceCollectionExtensions.cs
+++ Mc2.CrudTest.Presentation/Server/Extensions/ServiceCollectionExtensions.cs
@@ -8,8 +8,19 @@ namespace Mc2.CrudTest.Presentation.Server.Extensions
         internal static IServiceCollection AddDatabase(
             this IServiceCollection services,
             IConfiguration configuration)
-            => services
-                .AddDbContext<CrudDbContext>(options => options
-                    .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        {
+            return services.AddDbContext<CrudDbContext>(options =>
+            {
+                //options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                builder => builder.MigrationsAssembly(typeof(CrudDbContext).Assembly.FullName));
+
+                var dbContext = new CrudDbContext(options.Options);
+                if (dbContext.Database.GetPendingMigrations().Any())
+                {
+                    dbContext.Database.Migrate();
+                }
+            });
+        }
     }
 }

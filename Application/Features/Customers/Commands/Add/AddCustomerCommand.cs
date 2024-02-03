36fe06185511f3268840c20da8e6ed25e2179047
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 17:02:49 +0330

    add Add mediatr for customer

---
+++ Application/Features/Customers/Commands/Add/AddCustomerCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.Models;
+using Mc2.CrudTest.Presentation.Shared;
+using MediatR;
+
+namespace Application.Features.Customers.Commands.Add
+{
+    public class AddCustomerCommand : IRequest<Result<int>>
+    {
+        public CreateCustomerDTO Customer { get; set; }
+    }
+}

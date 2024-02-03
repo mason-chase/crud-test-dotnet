7ec0288aace757cddb95b18afd969527128eb4e6
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 17:03:10 +0330

    add Edit mediatr for customer

---
+++ Application/Features/Customers/Commands/Edit/EditCustomerCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Models;
+using Mc2.CrudTest.Presentation.Shared;
+using MediatR;
+
+namespace Application.Features.Customers.Commands.Edit
+{
+    public class EditCustomerCommand : IRequest<Result<int>>
+    {
+
+        public UpdateCustomerDTO Customer { get; set; }
+    }
+}

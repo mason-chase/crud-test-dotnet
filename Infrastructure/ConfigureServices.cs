998426f5b343df6c66e9be0f39450091e1fb7447
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 16:16:36 +0330

    add ConfigureServices in Infrastructure and register ICustomerRepository and implementation

---
+++ Infrastructure/ConfigureServices.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces.Repositories;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            return services.AddTransient<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}

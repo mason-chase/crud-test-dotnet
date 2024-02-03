acbef0a29f9ca8c826f8727808461772a86a2c80
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-03 16:14:45 +0330

    add ServiceCollectionExtensions in application and register Aoutomapper and MediatR in Application module

---
+++ Application/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.NetworkInformation;
+using System.Reflection;
+
+namespace Application.Configuration
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        }
+    }
+}

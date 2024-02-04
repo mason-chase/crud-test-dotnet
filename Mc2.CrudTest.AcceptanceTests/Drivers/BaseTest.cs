f436e304423f64df8d341c1f5f4253356fa5f74a
MohammadMobasher <m.mobasher.z@gmail.com>
2024-02-04 19:02:12 +0330

    create BaseTest in Test module

---
+++ Mc2.CrudTest.AcceptanceTests/Drivers/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using Mc2.CrudTest.Presentation;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.AcceptanceTests.Drivers
+{
+    public abstract class BaseTest
+    {
+        public HttpClient Client { get; set; }
+        public string ApiUri { get; set; }
+
+        public BaseTest()
+        {
+            var appFactory = new WebApplicationFactory<Program>();
+            Client = appFactory.CreateClient();
+            ApiUri = Client?.BaseAddress?.AbsoluteUri;
+        }
+    }
+}

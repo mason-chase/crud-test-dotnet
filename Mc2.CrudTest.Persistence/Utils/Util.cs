eb55a218e9fc57453505188a6ced959263de17df
atakankul <atakan.kul@kocholding.onmicrosoft.com>
2024-03-25 15:52:17 +0300

    project structure created, references added, mongodb connection added

---
+++ Mc2.CrudTest.Persistence/Utils/Util.cs
@@ -0,0 +1,7 @@
+namespace Mc2.CrudTest.Persistence.Utils;
+
+public static class Util
+{
+    public const string MongoDbConnectionString = "mongodb+srv://atakankul:Atiba33.@atakan.x9eklfx.mongodb.net/?retryWrites=true&w=majority&appName=atakan";
+    public const string CustomerCollection = "customers";
+}

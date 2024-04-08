1c9e04ede07a6982334d8c6d8ee86cbab422920a
Mohsen Heshmati <info@zagroscity.com>
2024-04-08 20:08:18 +0330

    Setup Program.CS of Endpint.Api, Configuration of NLog and Swagger

--- Src/Presentation/Endpoint.Api/Endpoint.Api/Program.cs
+++ Src/Presentation/Endpoint.Api/Endpoint.Api/Program.cs
@@ -1,25 +1,98 @@
-var builder = WebApplication.CreateBuilder(args);
+using Application;
+using Microsoft.AspNetCore.HttpLogging;
+using NLog.Web;
+using Persistence;
 
-// Add services to the container.
+NLog.Logger _nlog = NLog.LogManager.GetCurrentClassLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseNLog();
+
+    AddServices(builder);
+
+    var app = builder.Build();
+
+    AddConfigurations(app, builder);
+
+    app.Run();
+}
+catch (Exception e)
+{
+    _nlog.Error(e, e.Message);
 }
 
-app.UseHttpsRedirection();
+#region ServiceMethods
 
-app.UseAuthorization();
+void AddServices(WebApplicationBuilder builder)
+{
+    GeneralServices(builder);
+    LayersServices(builder);
+}
 
-app.MapControllers();
+void GeneralServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers();
 
-app.Run();
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.CustomSchemaIds(type => type.ToString());
+    });
+
+    builder.Services.AddHttpLogging(logging =>
+    {
+        logging.LoggingFields = HttpLoggingFields.All;
+    });
+}
+
+void LayersServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddApplicationLayer();
+    builder.Services.AddPersistenceInfrastructure(builder.Configuration);
+}
+
+#endregion ServiceMethods
+
+#region ConfigureMethods
+
+void AddConfigurations(WebApplication app, WebApplicationBuilder builder)
+{
+    GeneralConfiguration(app, builder);
+    ConfigureEndPoints(app);
+}
+void GeneralConfiguration(WebApplication app, WebApplicationBuilder builder)
+{
+    app.Use(async (context, next) =>
+    {
+        context.Request.EnableBuffering();
+        await next();
+    });
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+            options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+        });
+    }
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+}
+
+void ConfigureEndPoints(WebApplication app)
+{
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+}
+
+#endregion ConfigureMethods

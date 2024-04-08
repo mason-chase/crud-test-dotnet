6fac2aee67bc673bb203632298b9541d03bc4019
Mohsen Heshmati <info@zagroscity.com>
2024-04-08 20:14:40 +0330

    Adding GoogleLibPhoneNumber Nuget, Implement PhoneNumberService, Base Responses, and Setup Customer Controller

---
+++ Src/Core/Application/Application/Services/PhoneNumberService.cs
@@ -0,0 +1,34 @@
+﻿using Application.Contracts.Application;
+using PhoneNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class PhoneNumberService : IPhoneNumberService
+    {
+        private readonly PhoneNumberUtil _phoneUtil;
+
+        public PhoneNumberService()
+        {
+            _phoneUtil = PhoneNumberUtil.GetInstance();
+        }
+
+        public async Task<bool> IsValid(string phoneNumber)
+        {
+            try
+            {
+                var parsedNumber = _phoneUtil.Parse(phoneNumber, null);
+                return _phoneUtil.IsValidNumber(parsedNumber) &&
+                       _phoneUtil.GetNumberType(parsedNumber) == PhoneNumberType.MOBILE;
+            }
+            catch (NumberParseException)
+            {
+                return false;
+            }
+        }
+    }
+}
